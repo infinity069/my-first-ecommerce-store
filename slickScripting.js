de27ae764778220c8920e1f355db2d19d0a4614e
Pro-Dev <shittujamiy@gmail.com>
2024-08-18 12:35:52 +0100

    The conflict issues done

--- slickScripting.js
+++ slickScripting.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $('.slickSlider').slick({
     centerMode: true,
     slidesToShow: 4,
@@ -22,29 +21,4 @@ $('.slickSlider').slick({
         }
       }
     ]
-=======
-$('.slickSlider').slick({
-    centerMode: true,
-    slidesToShow: 4,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      }
-    ]
->>>>>>> da2ae80a4f14c65ccf278e4a70fa6a085dbaa54e
   });
--- slideScript.js
+++ slideScript.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: "auto",
   centeredSlides: false,
@@ -63,70 +62,4 @@ var swiper = new Swiper(".mobileshow", {
         spaceBetween: 10,
       }
     }
-=======
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: "auto",
-  centeredSlides: false,
-  spaceBetween: 15,
-  initialSlide: 1,
-  centeredSlides: true,
-  loop: true,
-  keyboard: {
-    enabled: true,
-  }
-});
-
-var swiper = new Swiper(".mobileshow", {
-    slidesPerView: 'auto',
-    spaceBetween: 10,
-    loop: true,
-    initialSlide: 1,
-    keyboard: {
-      enabled: true,
-    }
-
-  });
-  
-  var swiper = new Swiper(".filterSlide", {
-    slidesPerView: "auto",
-    spaceBetween: 10,
-    initialSlide: 0,
-    loop: false,
-    container: false,
-    centeredSlides: false,
-    keyboard: {
-      enabled: true,
-    },
-
-    breakpoints: {
-      320: {
-        slidesPerView: 1.2,
-        spaceBetween: 10,
-      }
-    },
-    breakpoints: {
-      400: {
-        slidesPerView: 2,
-        spaceBetween: 10,
-      }
-    },
-    breakpoints: {
-      640: {
-        slidesPerView: 2.5,
-        spaceBetween: 10,
-      }
-    },
-    breakpoints: {
-      768: {
-        slidesPerView: 3.2,
-        spaceBetween: 10,
-      }
-    },
-    breakpoints: {
-      992: {
-        slidesPerView: 4.2,
-        spaceBetween: 10,
-      }
-    }
->>>>>>> da2ae80a4f14c65ccf278e4a70fa6a085dbaa54e
   });
